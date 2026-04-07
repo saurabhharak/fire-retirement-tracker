d10298dd210f10e9807f0d26e456570b0a225f9b
saurabhharak <saurabhharak@users.noreply.github.com>
2026-04-07 19:33:24 +0530

    fix: remove unused MetricCard import from SipDisciplineWidget

--- frontend/src/components/dashboard/SipDisciplineWidget.tsx
+++ frontend/src/components/dashboard/SipDisciplineWidget.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { useSipLog } from "../../hooks/useSipLog";
-import { MetricCard } from "../MetricCard";
 import { formatRupees } from "../../lib/formatIndian";
 import { MONTH_NAMES } from "../../lib/constants";
 
